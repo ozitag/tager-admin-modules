fb9502b35d16844b4ac37f19d27e73a189db4808
Vital Ozierski <vital@ozerich.com>
2023-04-30 00:33:02 +0200

    fix build

--- packages/ui/src/components/OptionsSwitcher/index.ts
+++ packages/ui/src/components/OptionsSwitcher/index.ts
@@ -1,5 +1,3 @@
-export {
-  default as OptionsSwitcher,
-  type OptionsSwitcherOption,
-} from "./OptionsSwitcher.vue";
+export { default as OptionsSwitcher } from "./OptionsSwitcher.vue";
+export type { OptionsSwitcherOption } from "./OptionsSwitcher.vue";
 export { default as OptionsSwitcherTrueFalse } from "./OptionsSwitcherTrueFalse.vue";
